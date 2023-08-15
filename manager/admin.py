5ad549dbc90767bbe09c30ba8312277d1932e294
Munkoor <eee1lvlboss@gmail.com>
2023-08-15 12:59:17 +0300

    Registered my all models in admin.py

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from manager.models import Position, TaskType, Task, Worker
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["name", "deadline", "is_completed"]
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    search_fields = ["username", "first_name", "last_name"]
+
+
+

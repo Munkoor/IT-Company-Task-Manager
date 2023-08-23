ecec5a45523c04134a65691ebc5057f1154cbca2
Munkoor <eee1lvlboss@gmail.com>
2023-08-23 18:52:01 +0300

    set up files аÑfor deploньÑyment

--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 

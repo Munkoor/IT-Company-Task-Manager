c09dcb58c7f7d542be8200972fb435680648fd0e
Munkoor <eee1lvlboss@gmail.com>
2023-08-24 00:43:52 +0300

    Rewrote correct allowed host

--- Task_Manager/settings.py
+++ Task_Manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://it-task-manager-rrau.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "it-task-manager-rrau.onrender.com"]
 
 
 # Application definition
